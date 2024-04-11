2b8874df68dacc5e4bcba437f19ab29846049339
yPauloVitor <145620343+yPauloVitor@users.noreply.github.com>
2024-04-11 01:41:07 -0300

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,13 @@
+//script dos icones test
 $(document).ready(function() {
     
     $(".icon-wrapper").click(function() {
-    
         var icon = $(this).find("i");
-        
-        
-        if (icon.hasClass("bi-bookmark-check")) {
-            
+        if (icon.hasClass("bi-bookmark-check")) { 
             icon.toggleClass("icon-checked");
         } else if (icon.hasClass("bi-book")) {
-            
             icon.toggleClass("icon-reading");
         }
     });
 });
-
 
